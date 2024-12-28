6ecf4aa11a0e0683f7d1b4ff17aa1725b2eb28a7
IaraK <141530772+IaraK@users.noreply.github.com>
2024-12-28 19:40:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+{
+    var botoes0 = document.querySelector(".botao0");
+        botoes0.style.background="white";
+        
+        botoes0.addEventListener("mouseover", e =>{
+            botoes0.style.background="pink";
+        });
+        
+        botoes0.addEventListener("mouseout", e =>{
+            botoes0.style.background="white";
+        });
+        }
+
+{
+    var botoes6 = document.querySelector(".botao6");
+    botoes6.style.background="white";
+            
+    botoes6.addEventListener("mouseover", e =>{
+        botoes6.style.background="pink";
+    });
+            
+    botoes6.addEventListener("mouseout", e =>{
+        botoes6.style.background="white";
+    });
+    }
+
+{
+var botoes1 = document.querySelector(".botao1");
+botoes1.style.background="rgba(248, 242, 247, 0.911)";
+
+botoes1.addEventListener("mouseover", e =>{
+    botoes1.style.background="pink";
+});
+
+botoes1.addEventListener("mouseout", e =>{
+    botoes1.style.background="rgba(248, 242, 247, 0.911)";
+});
+}
+
+{
+var botoes2 = document.querySelector(".botao2");
+botoes2.style.background="white";
+
+botoes2.addEventListener("mouseover", e =>{
+    botoes2.style.background="pink";
+});
+
+botoes2.addEventListener("mouseout", e =>{
+    botoes2.style.background="white";
+});
+}
+
+{
+var botoes3 = document.querySelector(".botao3");
+botoes3.style.background="white";
+
+botoes3.addEventListener("mouseover", e =>{
+    botoes3.style.background="pink";
+});
+
+botoes3.addEventListener("mouseout", e =>{
+    botoes3.style.background="white";
+});
+}
+{
+var botoes4 = document.querySelector(".botao4");
+botoes4.style.background="white";
+
+botoes4.addEventListener("mouseover", e =>{
+    botoes4.style.background="purple";
+});
+
+botoes4.addEventListener("mouseout", e =>{
+    botoes4.style.background="white";
+});
+}
+
+{
+var botoes5 = document.querySelector(".botao5");
+botoes5.style.background="white";
+    
+botoes5.addEventListener("mouseover", e =>{
+    botoes5.style.background="purple";
+});
+    
+botoes5.addEventListener("mouseout", e =>{
+    botoes5.style.background="white";
+});
+}
+
+
+
